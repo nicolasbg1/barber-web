4f2421de71e6c667a13443a00444941e16389f89
João Pedro <joao.veo77@gmail.com>
2024-02-18 02:26:03 -0300

    fix modified file import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import GlobalStyles from './global-styles/global-style.tsx'
+import GlobalStyles from './styles/global.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
