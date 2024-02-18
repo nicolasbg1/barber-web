f671ae02c69873084f55e24c0b41e33566a8f2c3
João Pedro <joao.veo77@gmail.com>
2024-02-18 02:33:34 -0300

    files modified for layout and automatic deletion

--- src/components/team/index.tsx
+++
@@ -1,3 +0,0 @@
-import {} from './styles'
-
-export default Team
--- src/global-styles/colors-global.tsx
+++
@@ -1,6 +0,0 @@
-export const headerbg = '#04050C'
-export const buttonbg = '#F0B35B'
-export const buttonhover = '#d9a864'
-export const aboutbg = '#0D121E'
-export const servicesbg = '#'
-export const teambg = '#080B16'
